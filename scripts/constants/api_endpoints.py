28f73bb466a66826b0a779b4a83435a0f89f4c9a
Moganth K Kumar <moganthkumar.k@corp.knowledgelens.com>
2025-04-23 12:14:42 +0530

    fixed some minor issues

--- scripts/constants/api_endpoints.py
+++ scripts/constants/api_endpoints.py
@@ -1,6 +1,6 @@
 class Endpoints:
     AUTH_SIGNUP = "/auth/signup"
-    AUTH_LOGIN = "/login"
+    AUTH_LOGIN = "/auth/login"
 
     RATE_LIMIT_GET = "/rate-limit/{username}"
     RATE_LIMIT_SET = "/rate-limit/{username}/set"
